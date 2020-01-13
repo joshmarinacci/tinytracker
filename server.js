9a677d66ab34773f6e1afbdb5f62c297da2b7002
Glitch (joshmarinacci-tinytracker) <none>
2020-01-13 22:33:52 +0000

    Import of joshmarinacci/tinytracker

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ var Engine = require('tingodb')(),
 const cors = require('cors');
 
 const app = express()
-// app.use(bodyParser)
+app.use(bodyParser.json({limit:'20MB'}))
 app.use(cors())
 
 const PORT = 3000
+const PASSCODE = "foobarbaz"
 
 const DBEngine = new Engine.Db('.', {});
 const db = DBEngine.collection("events.db");
@@ -37,32 +38,35 @@ collection.insert([
 })
 */
 
-db.find({}, function(err, item) {
-    item.each((err,doc) => {
-        console.log(doc)
-    })
-});
+// db.find({}, function(err, item) {
+//     item.each((err,doc) => {
+//         console.log(doc)
+//     })
+// });
 
 app.get('/',(req,res)=>{
     res.send("this is the index page")
-  console.log('sent the index page')
 })
 // receive an event composed of the type and url in the query
-app.post('/event',(req,res)=>{
+app.get('/event',(req,res)=>{
     const {type,url} = req.query
-    console.log("getting an event",type,url)
+    console.log("got ",type,url)
     if(!type || !url) return res.status(400).json({status:'error',message:'missing parameters'})
     res.json({status:'success',message:`tracked ${type} at ${url}`})
     db.insert([{type:type, url:url, date: Date.now()}])
 })
 
-app.get('/data',(req,res)=>{
+app.post('/data.json',(req,res)=>{
+    console.log("body is",req.body)
+    if(!req.body || req.body.passcode !== PASSCODE) return res.status(400).json({status:'error',message:'invalid'})
     db.find({},(err,item)=>{
         item.toArray((err,items)=>{
             res.status(200).json(items).end()
         })
     })
 })
+
+app.use('/admin',express.static('admin'))
 
 app.use((req,res)=> res.status(400).end('invalid request'))
 app.listen(PORT,()=>console.log(`running tiny tracker on port ${PORT}`))
